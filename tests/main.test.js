ce4ca04429ee9ddb2c77b010595f376d3866ee18
dmitriyakkerman <darksider22023103>
2020-07-01 20:39:34 +0300

    тесты про проверке элемента

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,19 @@
-const Dom = require('../src/js/dom-util');
+const $d = require('../src/js/dom-util');
 
-test('Dom is true', () => {
-    expect(Dom).toBeTruthy();
-});
+describe('$d should return element', () => {
+
+  document.body.innerHTML = `
+    <div id="box" class="box"></div>
+  `;
+
+  test('$d should return element', () => {
+
+    expect($d('.box').selector.tagName).toBe("DIV");
+    expect($d('.box').selector.className).toBe("box");
+    expect($d('#box').selector.id).toBe("box");
+
+    $d('.box').addClass('box2');
+    expect($d('.box').hasClass('box2')).toBeTruthy();
+  });
+
+})
