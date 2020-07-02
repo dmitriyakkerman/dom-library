8725ffa17277f119ab6bca9236f39402bd5c3077
dmitriyakkerman <darksider22023103>
2020-07-02 17:19:34 +0300

    DomElement + DomMediator

--- src/js/dom-util.js
+++ src/js/dom-util.js
@@ -8,20 +8,23 @@
   }
 }(typeof self !== 'undefined' ? self : this, function () {
 
-  class DomUtil {
+  class DomElement {
     constructor(selector) {
-      this.selector = document.querySelector(selector);
+      this.selector = document.querySelectorAll(selector);
+      this.length = this.selector.length;
     }
 
-    html(html) {
-      this.selector.innerHTML = html;
+     html(html) {
+       this.selector.innerHTML = html;
 
-      return this;
-    }
+       return this;
+     }
 
-    clear() {
-      this.html('');
-    }
+     clear() {
+       this.html('');
+
+       return this;
+     }
 
     text(text) {
       this.selector.textContent = text;
@@ -29,92 +32,171 @@
       return this;
     }
 
-    addClass(className) {
-      this.selector.classList.add(className);
+     addClass(className) {
+       this.selector.classList.add(className);
 
-      return this;
+       return this;
+     }
+
+     removeClass(className) {
+       this.selector.classList.remove(className);
+
+       return this;
+     }
+
+     toggleClass(className) {
+       if (this.selector.classList.contains(className)) {
+         this.selector.classList.remove(className);
+       }
+       else {
+         this.selector.classList.add(className);
+       }
+
+       return this;
+     }
+
+     hasClass(className) {
+       if (this.selector.classList.contains(className)) {
+         return true;
+       }
+       else {
+         return false;
+       }
+
+       return this;
+     }
+
+     attr(attrName, attrValue) {
+       if (!attrValue) {
+         return this.selector.getAttribute('data-' + attrName);
+       }
+       else {
+         return this.selector.setAttribute('data-' + attrName, attrValue);
+       }
+
+       return this;
+     };
+
+     remove() {
+       this.selector.remove();
+
+       return this;
+     }
+
+     prepend(element) {
+       this.selector.prepend(element);
+
+       return this;
+     }
+
+     append(element) {
+       this.selector.append(element);
+
+       return this;
+     }
+
+     previous() {
+       return this.selector.previousElementSibling;
+     }
+
+     next() {
+       return this.selector.nextElementSibling;
+     }
+
+     parent() {
+       return this.selector.parentElement;
+     }
+
+     firstChild() {
+       return this.selector.firstElementChild;
+     }
+
+     lastChild() {
+       return this.selector.lastElementChild;
+     }
+
+     on(eventType, callback) {
+       this.selector.addEventListener(eventType, callback);
+     }
+  }
+
+  class DomMediator {
+    constructor(DomElement) {
+      this.DomElement = DomElement;
+      this.length = this.DomElement.length;
+    }
+
+    html(html) {
+      this.DomElement.html(html);
+    }
+
+    clear() {
+      this.DomElement.html('');
+    }
+
+    text(text) {
+      this.DomElement.text(text);
+    }
+
+    addClass(className) {
+      this.DomElement.addClass(className);
     }
 
     removeClass(className) {
-      this.selector.classList.remove(className);
-
-      return this;
+      this.DomElement.remove(className);
     }
 
     toggleClass(className) {
-      if (this.selector.classList.contains(className)) {
-        this.selector.classList.remove(className);
-      }
-      else {
-        this.selector.classList.add(className);
-      }
-
-      return this;
+      this.DomElement.toggleClass(className);
     }
 
     hasClass(className) {
-      if (this.selector.classList.contains(className)) {
-        return true;
-      }
-      else {
-        return false;
-      }
+      this.DomElement.hasClass(className)
     }
 
     attr(attrName, attrValue) {
-      if (!attrValue) {
-        return this.selector.getAttribute('data-' + attrName);
-      }
-      else {
-        return this.selector.setAttribute('data-' + attrName, attrValue);
-      }
+      this.DomElement.attr(attrName, attrValue);
     };
 
     remove() {
-      this.selector.remove();
-
-      return this;
+      this.DomElement.remove();
     }
 
     prepend(element) {
-      this.selector.prepend(element);
-
-      return this;
+      this.DomElement.prepend(element);
     }
 
     append(element) {
-      this.selector.append(element);
-
-      return this;
+      this.DomElement.append(element);
     }
 
     previous() {
-      return this.selector.previousElementSibling;
+      this.DomElement.previous();
     }
 
     next() {
-      return this.selector.nextElementSibling;
+      this.DomElement.next();
     }
 
     parent() {
-      return this.selector.parentElement;
+      this.DomElement.parent();
     }
 
     firstChild() {
-      return this.selector.firstElementChild;
+      this.DomElement.firstChild();
     }
 
     lastChild() {
-      return this.selector.lastElementChild;
+      this.DomElement.lastChild();
     }
 
     on(eventType, callback) {
-      this.selector.addEventListener(eventType, callback);
+      this.DomElement.on(eventType, callback);
     }
   }
 
   function $d(selector) {
-    return new DomUtil(selector);
+    return new DomMediator(new DomElement(selector));
   }
 
   $d.create = function (tagName, className) {
@@ -125,18 +207,6 @@
     }
 
     return element;
-  };
-
-  $d.find = function (selector) {
-    const element = document.querySelector(selector);
-
-    return element;
-  };
-
-  $d.findAll = function (selectors) {
-    const elements = document.querySelectorAll(selectors);
-
-    return elements;
   };
 
   window.$d = $d;
